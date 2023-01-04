bbaa9dd305206afb7c865aa39274cca423b7bfe5
MuhsinCanY <96295315+MuhsinCanY@users.noreply.github.com>
2023-01-04 20:44:37 +0300

    Odev1

---
+++ Techcareer.net_Odev_1.playground/Contents.swift
@@ -0,0 +1,95 @@
+import UIKit
+
+let sehir: String = "Ankara"
+let ulke: String = "Türkiye"
+let telefon: String = "5537171717"
+let postaKodu: Int = 06190
+let email: String = "tectcareer@techcareer.net"
+let meslek: String = "iOS Developer"
+let stokMiktari: Int = 1_000_000
+let musteriAdi: String = "Mehmet"
+let bakiye: Int = 15_489
+let dogumGunu: String = "07/07/1998"
+let maas: Int = 8_500
+let medeniDurum: String = "Bekar"
+let urunYorum: String = "Çok rahat. Tam beden alabilirsiniz"
+let ödemeTarihi: String = "02/02/2023"
+let ödeme: Int = 54_388
+let siparisAdeti: Int = 455
+let arabaModeli: Int = 2021
+let kitapAdi: String = "Dune"
+let yayinlamaTarihi: String = "04/01/2023"
+let indirimMiktari: Int = 25
+let odaSayisi: Int = 4
+let enlem: Double = 39.9272
+let boylam: Double = 32.8644
+let ürünAdi: String = "Polar Outdoor Kapşonlu"
+let yemekFiyati: Int = 80
+let marka: String = "GENIUS"
+let muzikAdi: String = "Yolun Sonu Görünüyor"
+let videoSuresi: String = "5:38"
+let urunPuani: Double = 4.8
+let resimAdi: String = "photo_2023-01-04 03.49.00"
+let dosyaFormati: String = ".jpeg"
+let renk: String = "Firebrick1"
+let renkKodu: String = "#FF3030"
+let telefonModeli: String = "Xiaomi"
+let ekranBoyutu: Double = 6.53
+let agirlik: Int = 75
+let ulusalGun: String = "23/04/1920"
+let tatilGunu: String = "Pazar"
+let rezervasyonTarihi: String = "03/09/2023"
+let sokakAdi: String = "Güzelyaka"
+let otobusHatti: Int = 255
+let kalanDakika: Int = 14
+let takipKodu: Int = 6679711
+let kuponSuresi: Int = 98
+let kuponKodu: String = "muhsin10"
+let faturaAdresi: String = "Güzelyaka Mah. Şirin Sok. No: 1/5"
+
+print("sehir : \(sehir)")
+print("ülke : \(ulke)")
+print("telefon : \(telefon)")
+print("postaKodu : \(postaKodu)")
+print("email : \(email)")
+print("meslek : \(meslek)")
+print("stokMiktari : \(stokMiktari)")
+print("müsteriAdi : \(musteriAdi)")
+print("bakiye : \(bakiye)")
+print("dogumGünü : \(dogumGunu)")
+print("maas : \(maas)")
+print("medeniDurum : \(medeniDurum)")
+print("ürünYorum : \(urunYorum)")
+print("ödemeTarihi : \(ödemeTarihi)")
+print("ödeme : \(ödeme)")
+print("siparisAdeti : \(siparisAdeti)")
+print("arabaModeli : \(arabaModeli)")
+print("kitapAdi : \(kitapAdi)")
+print("yayinlamaTarihi : \(yayinlamaTarihi)")
+print("indirimMiktari : \(indirimMiktari)")
+print("odaSayisi : \(odaSayisi)")
+print("enlem : \(enlem)")
+print("boylam : \(boylam)")
+print("ürünAdi : \(ürünAdi)")
+print("yemekFiyati : \(yemekFiyati)")
+print("marka : \(marka)")
+print("müzikAdi : \(muzikAdi)")
+print("videoSüresi : \(videoSuresi)")
+print("ürünPuani : \(urunPuani)")
+print("resimAdi : \(resimAdi)")
+print("dosyaFormati : \(dosyaFormati)")
+print("renk : \(renk)")
+print("renkKodu : \(renkKodu)")
+print("telefonModeli : \(telefonModeli)")
+print("ekranBoyutu : \(ekranBoyutu)")
+print("agirlik : \(agirlik)")
+print("ulusalGün : \(ulusalGun)")
+print("tatilGünü : \(tatilGunu)")
+print("rezervasyonTarihi : \(rezervasyonTarihi)")
+print("sokakAdi : \(sokakAdi)")
+print("otobüsHatti : \(otobusHatti)")
+print("kalanDakika : \(kalanDakika)")
+print("takipKodu : \(takipKodu)")
+print("kuponSüresi : \(kuponSuresi)")
+print("kuponKodu : \(kuponKodu)")
+print("faturaAdresi : \(faturaAdresi)")
