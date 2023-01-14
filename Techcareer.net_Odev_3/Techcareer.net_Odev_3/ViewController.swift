c58d2e123b89e288cbe048c19c336ce2569bb74a
MuhsinCanY <96295315+MuhsinCanY@users.noreply.github.com>
2023-01-14 20:32:17 +0300

    Techcareer.net_Odev_3

---
+++ Techcareer.net_Odev_3/Techcareer.net_Odev_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Techcareer.net_Odev_3
+//
+//  Created by Muhsin Can Yılmaz on 13.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
